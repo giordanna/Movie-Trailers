ddce7cbd3a0b3752b7079a25149779a79f065e21
Giordanna De Gregoriis <gior.grs@gmail.com>
2018-09-03 21:08:20 -0300

    Adicionando uso de API

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Este projeto permite gerar um site estático de trailer de filmes usando python.
 
 ### Como instalar
 - Clone ou baixe este repositório;
+- Certifique-se de que possui a API de filmes tmdbsimple instalada, execute em seu terminal:
+```sh
+pip install tmdbsimple
+```
 - Execute o arquivo entretainment_center.py no seu editor de preferência ou através do terminal utilizando:
 ```sh
 python entretainment_center.py
@@ -11,21 +15,20 @@ python entretainment_center.py
 - Aguarde até que o arquivo fresh_tomatoes.html seja gerado. Ele será aberto automaticamente em seu navegador.
 **OBS:** certifique-se de que possui o python instalado.
 
-### Como modificar os filmes
-- Abra o arquivo entretainment_center.py no seu editor de preferência e adicione:
-```python
-nome_filme = media.Movie("Nome do filme",
-                         "História do filme",
-                         "Link do poster do filme",
-                         "Link do trailer no youtube do filme")
-```
-- Você pode apagar os outros que estão como amostra caso desejar;
-- Por fim, não esqueça de adicionar a lista de filmes para passar como argumento para o gerador do site:
-```python
-movies = [..., nome_filme]
-fresh_tomatoes.open_movies_page(movies)
-```
+### Chave da API
+Você vai precisar de uma chave da API do The Movie Database. Para obter a chave:
 
-### Todos
+1) [Registre-se](https://www.themoviedb.org/account/signup) no site;
+2) [Faça login](https://www.themoviedb.org/login) na sua conta;
+3) Selecione a seção API no canto esquerdo em "Configurações" da sua conta;
+4) Clique no link para gerar a chave e siga as instruções.
+
+### Como modificar os filmes
+- Abra o arquivo entretainment_center.py no seu editor de preferência e modifique:
+```python
+response = movies.now_playing()
+```
+- Verifique as demais funções disponíveis no código-fonte do [tmdbsimple](https://github.com/celiao/tmdbsimple).
+
+### TODOs
  - Refazer o layout do site do zero
- - Utilizar uma API para listar filmes
--- entretainment_center.py
+++ entretainment_center.py
@@ -1,36 +1,46 @@
 # -*- coding: utf-8 -*-
+import media
+import fresh_tomatoes
+import time
+import tmdbsimple as tmdb
 
-import media, fresh_tomatoes
+tmdb.API_KEY = 'CHAVE_API'
 
-mad_max = media.Movie("Mad Max: Fury Road",
-                       "Max acredita que a melhor forma de sobreviver é não depender de mais ninguém além de si próprio",
-                       "https://upload.wikimedia.org/wikipedia/pt/2/23/Max_Mad_Fury_Road_Newest_Poster.jpg",
-                       "https://www.youtube.com/watch?v=hEJnMQG9ev8")
+# faz busca dos filmes que estão em cartaz nos cinemas
+movies = tmdb.Movies()
+response = movies.now_playing()
+movies_array = []
 
-pantera_negra = media.Movie("Pantera Negra",
-                       "Depois dos eventos de Capitão América: Guerra Civil, após a morte de T'Chaka, seu filho T'Challa retorna a Wakanda para assumir o trono",
-                       "https://upload.wikimedia.org/wikipedia/pt/9/90/Pantera_Negra_%28poster%29.jpg",
-                       "https://www.youtube.com/watch?v=xjDjIWPwcPU")
+for m in movies.results:
 
-deadpool_2 = media.Movie("Deadpool 2",
-                       "Depois de trabalhar com sucesso como o mercenário Deadpool por dois anos, Wade Wilson não consegue matar um de seus alvos em seu aniversário com sua namorada Vanessa",
-                       "https://upload.wikimedia.org/wikipedia/pt/4/46/Deadpool_2.jpg",
-                       "https://www.youtube.com/watch?v=1P9OzWX6nzE")
+    print("Gerando entrada de filme para '" + m['title'] + "' em "
+          + time.ctime())
 
-logan = media.Movie("Logan",
-                       "Em 2029, os mutantes estão à beira da extinção, com nenhum novo mutante tendo nascido em 25 anos",
-                       "https://upload.wikimedia.org/wikipedia/pt/2/2d/Filme_Logan_2017.jpg",
-                       "https://www.youtube.com/watch?v=KPND6SgkN7Q")
+    # constrói o link para o poster do filme
+    poster_url = 'https://image.tmdb.org/t/p/original' + m['poster_path']
+    trailer_url = ""
 
-ghost_shell = media.Movie("Ghost in the Shell",
-                       "Num futuro próximo, a maior parte dos seres humanos possui implantes cibernéticos, aprimorando várias características como visão, força e inteligência",
-                       "https://upload.wikimedia.org/wikipedia/pt/d/d1/Ghost_in_the_Shell.jpg",
-                       "https://www.youtube.com/watch?v=G4VmJcZR0Yg")
+    # busca entre os videos relacionados do filme o que for do tipo "Trailer"
+    # e constrói o link para o video do youtube
+    movie = tmdb.Movies(m['id'])
+    response = movie.videos()
 
-jogo_imitacao = media.Movie("O Jogo da Imitação",
-                       "Em 1951, dois policiais investigam o matemático Alan Turing após o que aparenta ser uma invasão de sua casa. Suspeitando de seu comportamento e falta de registros de guerra, decidem interrogar Turing com medo que ele seja um espião",
-                       "https://upload.wikimedia.org/wikipedia/pt/1/1a/O_Jogo_da_Imita%C3%A7%C3%A3o.jpg",
-                       "https://www.youtube.com/watch?v=YIkKbMcJL_4")
+    for v in movie.results:
+        if v['type'] == 'Trailer' and v['site'] == 'YouTube':
+            trailer_url = 'https://www.youtube.com/watch?v=' + v['key']
+            break
 
-movies = [deadpool_2, ghost_shell, jogo_imitacao, logan, mad_max, pantera_negra]
-fresh_tomatoes.open_movies_page(movies)
+    # cria o objeto do filme e adiciona na lista
+    movies_array.append(
+            media.Movie(
+                    m['title'],
+                    m['overview'],
+                    poster_url,
+                    trailer_url
+                    )
+            )
+
+print("\nGeração da página concluída em " + time.ctime() + "!" )
+
+# cria a página com a lista de filmes como entrada
+fresh_tomatoes.open_movies_page(movies_array)
